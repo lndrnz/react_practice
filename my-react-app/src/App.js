2d2d93b36f25b2e5debe9f6f034e614dc6f3a5c3
Lander Nunez <landernunez@gmail.com>
2023-08-07 20:38:23 -0700

    stuff

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+<div className='flex'>
+  <div className='red '></div>
+  <div className='blue '></div>
+  <div className='green '></div>
+  <div className='yellow '></div>
+  <div className='purple'></div>
+  </div>
+
+  )
+}
+
+export default App;
